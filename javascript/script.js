aeb973901967edb46018865a10c1390bb9899229
jernej kozelj <kozeljernej@gmail.com>
2015-11-20 22:38:12 +0100

    First commit. Index, login, registration.

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+//funkcija za prikaz gadgetov s samodejnim scrollanjem
+
+$(document).ready(function(){
+    $("ul li:eq(1)").click(function(){
+		$('html,body').animate({
+	        scrollTop: '620'
+	    }, 1000, 'linear');
+    });
+});
